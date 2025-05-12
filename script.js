01049d2ff3503cb11ed2e54a9325d762767dddda
calypso254 <129347390+calypso254@users.noreply.github.com>
2025-05-12 07:10:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,23 +6,50 @@ async function sendToMake() {
 
   responseEl.textContent = "Sending...";
 
+  // Hardcoded list of personas for testAll mode
+  const allPersonas = [
+    "Stacy the Stylish Teacher",
+    "Claire the Corporate Queen",
+    "Sam the Journal Addict",
+    "Brittany the Bride-to-Be",
+    "Melissa the Pen Collector"
+  ];
+
   try {
     const res = await fetch("https://hook.us1.make.com/qv4nysn1ilxu1jx7ez67yampsgkpm671", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ persona, copy, testAll })
+      body: JSON.stringify({
+        persona,
+        copy,
+        testAll,
+        all_personas: testAll ? allPersonas : null
+      })
     });
 
     const data = await res.json();
 
-    const { score, reaction, suggestions, purchase_likelihood } = data;
+    // Check if response is an array (testAll mode)
+    if (Array.isArray(data)) {
+      responseEl.innerHTML = data.map(item => `
+        <hr>
+        <strong>Persona:</strong> ${item.persona}<br>
+        <strong>Score:</strong> ${item.score}<br>
+        <strong>Reaction:</strong> ${item.reaction}<br>
+        <strong>Suggestions:</strong> ${item.suggestions}<br>
+        <strong>Purchase Likelihood:</strong> ${item.purchase_likelihood}
+      `).join('');
+    } else {
+      // Single-persona response
+      const { score, reaction, suggestions, purchase_likelihood } = data;
 
-    responseEl.innerHTML = `
-      <strong>Score:</strong> ${score}<br>
-      <strong>Reaction:</strong> ${reaction}<br>
-      <strong>Suggestions:</strong> ${suggestions}<br>
-      <strong>Purchase Likelihood:</strong> ${purchase_likelihood}
-    `;
+      responseEl.innerHTML = `
+        <strong>Score:</strong> ${score}<br>
+        <strong>Reaction:</strong> ${reaction}<br>
+        <strong>Suggestions:</strong> ${suggestions}<br>
+        <strong>Purchase Likelihood:</strong> ${purchase_likelihood}
+      `;
+    }
   } catch (err) {
     responseEl.textContent = "Error: " + err.message;
   }
